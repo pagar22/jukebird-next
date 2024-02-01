389de50990e6497728c5a89964518e5551805677
pagar22 <aaryanpagar@hotmail.com>
2024-02-01 17:05:24 +0100

    feat: next conf for deploying static site to firebase

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={{background: "black", color: "white"}}>{children}</body>
     </html>
   );
 }
